b8c63ecd58cebbbed17336c2e8719ba6fc77ef79
JJ <jorge.lopresti01@gmail.com>
2025-09-10 21:53:29 +0200

    uppdate tf variables

--- infra/variables.tf
+++ infra/variables.tf
@@ -5,7 +5,19 @@ variable "storage_account"   { type = string }
 variable "key_vault_name"    { type = string }
 variable "acr_name"          { type = string }
 variable "appinsights_name"  { type = string }
-variable "compute_name"      { type = string  default = "cpu-cluster_j1" }
-variable "vm_size"           { type = string  default = "Standard_DS3_v2" }
-variable "max_nodes"         { type = number  default = 2 }
-variable "min_nodes"         { type = number  default = 0 }
+variable "compute_name"      {
+        type = string  
+        default = "cpu-cluster_j1" 
+    }
+variable "vm_size"           { 
+    type = string  
+    default = "Standard_DS3_v2" 
+    }
+variable "max_nodes"         { 
+    type = number  
+    default = 2 
+    }
+variable "min_nodes"         {
+    type = number  
+    default = 0 
+    }
